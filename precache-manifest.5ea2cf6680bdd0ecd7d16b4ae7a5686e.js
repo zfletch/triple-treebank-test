e1d756177b196ff1c9430b6d2f70682ac5d85243
zfletch <zfletch2@gmail.com>
2020-07-29 12:05:37 -0400

    Updates

--- precache-manifest.5ea2cf6680bdd0ecd7d16b4ae7a5686e.js
+++ precache-manifest.5ea2cf6680bdd0ecd7d16b4ae7a5686e.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "96e3466ed86ea705f839fedd777640b3",
+    "revision": "2a7ba7c251f7768192d147b44a8f65e9",
     "url": "/triple-treebank-test/index.html"
   },
   {
-    "revision": "6391d0c856bc68a4104a",
+    "revision": "60c70195d0158e262e66",
     "url": "/triple-treebank-test/static/css/2.0379961b.chunk.css"
   },
   {
-    "revision": "f545582ef8b30cabd925",
+    "revision": "8a20656f4779022153b1",
     "url": "/triple-treebank-test/static/css/main.17a19e8e.chunk.css"
   },
   {
-    "revision": "6391d0c856bc68a4104a",
-    "url": "/triple-treebank-test/static/js/2.ef37fcc9.chunk.js"
+    "revision": "60c70195d0158e262e66",
+    "url": "/triple-treebank-test/static/js/2.8ffad348.chunk.js"
   },
   {
     "revision": "40046c021c0025c71b5ec67323b92a85",
-    "url": "/triple-treebank-test/static/js/2.ef37fcc9.chunk.js.LICENSE.txt"
+    "url": "/triple-treebank-test/static/js/2.8ffad348.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f545582ef8b30cabd925",
-    "url": "/triple-treebank-test/static/js/main.1f8a65db.chunk.js"
+    "revision": "8a20656f4779022153b1",
+    "url": "/triple-treebank-test/static/js/main.0b545e4f.chunk.js"
   },
   {
     "revision": "9b13acdcdc8778601b35",
