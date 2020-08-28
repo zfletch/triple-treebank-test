4e1ab4654585a285850b60b64d1ed1546271b72f
zfletch <zfletch@users.noreply.github.com>
2020-08-28 20:29:30 +0000

    Deploying to gh-pages from  @ 51677954af55c566da743a6f17fa6b617e0c1633 🚀

--- precache-manifest.f30ffd934b28978cc0adc66c117bc0d0.js
+++ precache-manifest.f30ffd934b28978cc0adc66c117bc0d0.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a7ba7c251f7768192d147b44a8f65e9",
+    "revision": "96e3466ed86ea705f839fedd777640b3",
     "url": "/triple-treebank-test/index.html"
   },
   {
-    "revision": "60c70195d0158e262e66",
+    "revision": "6391d0c856bc68a4104a",
     "url": "/triple-treebank-test/static/css/2.0379961b.chunk.css"
   },
   {
-    "revision": "8a20656f4779022153b1",
+    "revision": "f545582ef8b30cabd925",
     "url": "/triple-treebank-test/static/css/main.17a19e8e.chunk.css"
   },
   {
-    "revision": "60c70195d0158e262e66",
-    "url": "/triple-treebank-test/static/js/2.8ffad348.chunk.js"
+    "revision": "6391d0c856bc68a4104a",
+    "url": "/triple-treebank-test/static/js/2.ef37fcc9.chunk.js"
   },
   {
     "revision": "40046c021c0025c71b5ec67323b92a85",
-    "url": "/triple-treebank-test/static/js/2.8ffad348.chunk.js.LICENSE.txt"
+    "url": "/triple-treebank-test/static/js/2.ef37fcc9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8a20656f4779022153b1",
-    "url": "/triple-treebank-test/static/js/main.0b545e4f.chunk.js"
+    "revision": "f545582ef8b30cabd925",
+    "url": "/triple-treebank-test/static/js/main.1f8a65db.chunk.js"
   },
   {
     "revision": "9b13acdcdc8778601b35",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/triple-treebank-test/precache-manifest.5ea2cf6680bdd0ecd7d16b4ae7a5686e.js"
+  "/triple-treebank-test/precache-manifest.f30ffd934b28978cc0adc66c117bc0d0.js"
 );
 
 self.addEventListener('message', (event) => {
